bbcff5c7c30fb46a0719af6e514ee148d3cbbcbb
Rahul <rahul.shial@gmail.com>
2020-11-04 15:19:37 +0000

    move setup input & handleUserInput to input.js

---
+++ input.js
@@ -0,0 +1,24 @@
+/**
+ * Setup User Interface
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  handleUserInput();
+  return stdin;
+};
+
+const handleUserInput = function() {
+  process.stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      console.log("Thanks for using me, ciao!");
+      process.exit();
+    }
+  });
+};
+
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,30 +1,10 @@
 // const net = require('net');
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 console.log('Connecting ...');
 connect();
 
-/**
- * Setup User Interface
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  handleUserInput();
-  return stdin;
-};
-
-const handleUserInput = function() {
-  process.stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      console.log("Thanks for using me, ciao!");
-      process.exit();
-    }
-  });
-};
-
 setupInput();
+
 
