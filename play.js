67f1a6ad676eaee339d68d22049eda631e210b08
Rahul <rahul.shial@gmail.com>
2020-11-04 15:15:02 +0000

    added stdin and event handler to play.js

--- play.js
+++ play.js
@@ -4,4 +4,27 @@ const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
 
+/**
+ * Setup User Interface
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  handleUserInput();
+  return stdin;
+};
+
+const handleUserInput = function() {
+  process.stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      console.log("Thanks for using me, ciao!");
+      process.exit();
+    }
+  });
+};
+
+setupInput();
 
