c6a4a5fbbcf1dd66cc55b0fdd6fa616dd75535b5
Rahul <rahul.shial@gmail.com>
2020-11-04 15:47:28 +0000

    cleaned up code

--- client.js
+++ client.js
@@ -25,17 +25,17 @@ const connect = function() {
   //   }
   // });
 
-  conn.on('connect', () => {
-    let numTimes = 0;
-    const interval = setInterval(() => {
-      numTimes++;
-      // conn.write('Move: up');
-      conn.write('Move: left');
-      if (numTimes === 10) {
-        return clearInterval(interval);
-      }
-    }, 250);
-  });
+  // conn.on('connect', () => {
+  //   let numTimes = 0;
+  //   const interval = setInterval(() => {
+  //     numTimes++;
+  //     // conn.write('Move: up');
+  //     conn.write('Move: left');
+  //     if (numTimes === 10) {
+  //       return clearInterval(interval);
+  //     }
+  //   }, 250);
+  // });
 
   // interpret incoming data as text
   conn.setEncoding('utf8');
