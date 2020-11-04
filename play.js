b3debf247c0ad5cd2a2f5f5d61210f58e353b04d
Rahul <rahul.shial@gmail.com>
2020-11-04 15:46:19 +0000

    captured conn return value from connect() to pass to setupInput

--- play.js
+++ play.js
@@ -3,8 +3,8 @@ const { connect } = require('./client');
 const { setupInput } = require('./input');
 
 console.log('Connecting ...');
-connect();
+const conn = connect();
 
-setupInput();
+setupInput(conn);
 
 
