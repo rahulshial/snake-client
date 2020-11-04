087ac2ca223f4344145c89702544f754199ffbd2
Rahul <rahul.shial@gmail.com>
2020-11-04 14:32:53 +0000

    Experimenting with SetTimeOut & SetInterval

--- client.js
+++ client.js
@@ -9,10 +9,34 @@ const connect = function() {
     host: 'localhost',
     port: 50541
   });
+  // initialize player name
   conn.on('connect', () => {
     conn.write('Name: RRS');
   });
   
+  //EXPERIMENTING WITH TIMEOUTS AND INTERVALS
+
+  // conn.on('connect', () => {
+  //   for (let i = 0; i < 10; i++) {
+  //     setTimeout(() => {
+  //       conn.write('Move: up');
+  //       // conn.write('Move: left');
+  //     }, i * 250);
+  //   }
+  // });
+
+  // conn.on('connect', () => {
+  //   let numTimes = 0;
+  //   const interval = setInterval(() => {
+  //     numTimes++;
+  //     // conn.write('Move: up');
+  //     conn.write('Move: left');
+  //     if (numTimes === 10) {
+  //       return clearInterval(interval);
+  //     }
+  //   }, 250);
+  // });
+
   // interpret incoming data as text
   conn.setEncoding('utf8');
   conn.on('data', (data) => {
